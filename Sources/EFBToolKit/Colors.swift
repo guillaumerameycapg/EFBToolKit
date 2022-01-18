1b6466d176a09ef5f41318e358538d038d87e751
Guillaume Ramey <guillaume.ramey@capgemini.com>
2022-01-18 11:07:07 +0100

    add Colors

---
+++ Sources/EFBToolKit/Colors.swift
@@ -0,0 +1,23 @@
+//
+//  Colors.swift
+//  
+//
+//  Created by Guillaume Ramey on 18/01/2022.
+//
+
+import SwiftUI
+
+@available(iOS 13.0, *)
+public extension Color {
+  static let lightText = Color(UIColor.lightText)
+  static let darkText = Color(UIColor.darkText)
+
+  static let label = Color(UIColor.label)
+  static let secondaryLabel = Color(UIColor.secondaryLabel)
+  static let tertiaryLabel = Color(UIColor.tertiaryLabel)
+  static let quaternaryLabel = Color(UIColor.quaternaryLabel)
+
+  static let systemBackground = Color(UIColor.systemBackground)
+  static let secondarySystemBackground = Color(UIColor.secondarySystemBackground)
+  static let tertiarySystemBackground = Color(UIColor.tertiarySystemBackground)
+}
